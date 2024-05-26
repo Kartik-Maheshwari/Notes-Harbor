157c5937714db358e14c4263b323088d0bb0dad9
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2024-05-26 19:23:51 +0530

    updated MainPage

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Card from './Card.jsx'
+import React from "react";
+import Card from "./Card.jsx";
 
-const Cards = ({data}) => {
+const Cards = ({ data }) => {
   return (
     <div className="flex flex-wrap justify-center gap-8">
-    {/* {data.map((card) => (
+      {/* {data.map((card) => (
       <Card
         key={card.id} // Add a unique key for each card (assuming an 'id' property)
         title={card.title}
@@ -12,10 +12,19 @@ const Cards = ({data}) => {
         description={card.description}
       />
     ))} */}
-    <Card/>
-   
-  </div>
-  )
-}
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
